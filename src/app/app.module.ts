ab076f5d8f73e2d8a5e1f95100f8c5dff30bf733
Cristina Fernández <mcferrod@gmail.com>
2017-07-11 13:15:41 +0200

    style css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { LeftComponent } from './common/component/left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LeftComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/common/component/left.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component ({
+  selector: 'app-left-navigation',
+  templateUrl: './left.component.html',
+  styleUrls: [
+    './left.component.css'
+  ]
+
+})
+export class LeftComponent{
+
+}
