7d7b0aa9edeb8bc157d4dd85e468f7baa5d28ae3
Cristina Fernández <mcferrod@gmail.com>
2017-07-12 11:58:42 +0200

    try add event click

--- src/app/common/component/left.component.ts
+++ src/app/common/component/left.component.ts
@@ -1,13 +1,20 @@
 import {Component} from '@angular/core';
 
+// import {TheRollingStonesPage} from './pages/the-rolling-stones/the-rolling-stones';
+
 @Component ({
   selector: 'app-left-navigation',
   templateUrl: './left.component.html',
   styleUrls: [
     './left.component.scss'
   ]
-
 })
-export class LeftComponent{
+export class LeftComponent {
+
+  // public goToTheRollingStonesPage;
+  //
+  // constructor() {
+  //   this.goToTheRollingStonesPage();
+  // }
 
 }
