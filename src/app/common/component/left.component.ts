774b505c47ace9d406d00179bd930d6bced14175
Cristina Fernández <mcferrod@gmail.com>
2017-07-14 13:23:27 +0200

    link page

--- src/app/common/component/left.component.ts
+++ src/app/common/component/left.component.ts
@@ -1,6 +1,15 @@
 import {Component} from '@angular/core';
+import { Routes}  from '@angular/router';
+import { } from '../../pages/the-rolling-stones/the-rolling-stones';
 
-// import {TheRollingStonesPage} from './pages/the-rolling-stones/the-rolling-stones';
+
+
+import { enableProdMode }  from '@angular/core';
+
+
+enableProdMode();
+
+
 
 @Component ({
   selector: 'app-left-navigation',
@@ -8,13 +17,22 @@ import {Component} from '@angular/core';
   styleUrls: [
     './left.component.scss'
   ]
+
 })
+
 export class LeftComponent {
 
-  // public goToTheRollingStonesPage;
-  //
-  // constructor() {
-  //   this.goToTheRollingStonesPage();
-  // }
-
 }
+
+@Routes([{
+  selector: 'page-navigation',
+  path: '../../pages/the-rolling-stones/the-rolling-stones',
+  component: TheRollingStonesPage}
+  ])
+
+export class TheRollingStonesPage {
+
+    constructor(private router:Routes) { }
+
+  title = 'my-app works!';
+  }
